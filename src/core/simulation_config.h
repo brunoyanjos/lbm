6315b7c63d9303bf462958268b1f1a5b3b691ee5
brunoyanjos <bruno.yan.anjos@gmail.com>
2026-04-07 19:59:21 -0300

    diminishing flow time

--- src/core/simulation_config.h
+++ src/core/simulation_config.h
@@ -3,5 +3,5 @@
 #include "geometry.h"
 #include "physics.h"
 
-constexpr int N_STEPS = real_t(250) * L_CHAR / U_MAX;
+constexpr int N_STEPS = real_t(100) * L_CHAR / U_MAX;
 constexpr int SAVE_INTERVAL = L_CHAR / U_MAX;
