f512ce524279489efb2bf4de57b884da7f060176
brunoyanjos <bruno.yan.anjos@gmail.com>
2026-06-01 16:35:47 -0300

    fixes on eval density sintax

--- src/lbm/boundary/fluid/eval_density.cuh
+++ src/lbm/boundary/fluid/eval_density.cuh
@@ -11,12 +11,12 @@ namespace boundary::fluid
     [[nodiscard]] __device__ __forceinline__ real_t eval_density(const Accumulator<RegOrder, Rec> &acc,
                                                                  const NodeMomentsFor<RegOrder, Rec, HighOrder> &M)
     {
-        const real_t rho_denominator = acc.rho.rho +
-                                       M.ux * acc.rho.ux + M.uy * acc.rho.uy +
-                                       M.ux * M.ux * acc.rho.uxux + M.ux * M.uy * acc.rho.uxuy +
-                                       M.uy * M.uy * acc.rho.uyuy +
-                                       M.mxx * acc.rho.mxx + M.mxy * acc.rho.mxy +
-                                       M.myy * acc.rho.myy;
+        real_t rho_denominator = acc.rho.rho +
+                                 M.ux * acc.rho.ux + M.uy * acc.rho.uy +
+                                 M.ux * M.ux * acc.rho.uxux + M.ux * M.uy * acc.rho.uxuy +
+                                 M.uy * M.uy * acc.rho.uyuy +
+                                 M.mxx * acc.rho.mxx + M.mxy * acc.rho.mxy +
+                                 M.myy * acc.rho.myy;
 
         if constexpr (RegOrder >= 3)
         {
@@ -27,7 +27,7 @@ namespace boundary::fluid
             {
                 rho_denominator += M.ux * M.mxx * acc.rho.uxmxx + M.uy * M.mxx * acc.rho.uymxx +
                                    M.ux * M.mxy * acc.rho.uxmxy + M.uy * M.mxy * acc.rho.uymxy +
-                                   M.ux * M.myy * acc.rho.uxmyy + M.uy * M.myy * acc.rho.uymyy
+                                   M.ux * M.myy * acc.rho.uxmyy + M.uy * M.myy * acc.rho.uymyy;
             }
         }
 
